522ae0bb07ba671d45d9a8b6aa92ad4edad1f7e1
AritzGarci06 <aritz.garcia@ikasle.aeg.eus>
2022-11-22 20:48:07 +0100

    Initial_Commit

---
+++ src/screens/Mapas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Mapas = () => {
+  return (
+    <View>
+      <Text>
+        Mapas
+      </Text>
+    </View>
+  )
+}
+
+export default Mapas;
---
+++ src/screens/Patinetes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Patinetes = () => {
+  return (
+    <View>
+      <Text>
+        Patinetes
+      </Text>
+    </View>
+  )
+}
+
+export default Patinetes;
