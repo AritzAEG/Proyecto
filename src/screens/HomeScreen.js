d519be1e9872d9e3419e3d96cf5e89a253b705e4
AritzGarci06 <aritz.garcia@ikasle.aeg.eus>
2022-11-29 21:08:48 +0100

    Initial_Commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,103 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import React, { useState} from "react";
+import { View, TouchableOpacity, StyleSheet, Animated, Image } from "react-native";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HomeScreen = () => {
-  return (
-      <ImageBackground source={require('../media/fondopatinete.png')} />
+
+  const [icon_1] = useState(new Animated.Value(40));
+  const [icon_2] = useState(new Animated.Value(40));
+  const [icon_3] = useState(new Animated.Value(40));
+
+  const [pop, setPop] = useState(false);
+
+  const popIn = () => {
+    setPop(true);
+    Animated.timing(icon_1, {
+      toValue: 130,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_2, {
+      toValue: 110,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_3, {
+      toValue: 130,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  const popOut = () => {
+    setPop(false);
+    Animated.timing(icon_1, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_2, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_3, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  return(
+    <View style={{
+      flex: 1
+    }}>
+      <Image source={require('../media/fondopatinete.png')} resizMode="cover" style={styles.image}/>
+      <Animated.View style={[styles.circle, { bottom: icon_1}]}>
+        <TouchableOpacity>
+          <MaterialCommunityIcons name="scooter" color='white' size={25} />
+        </TouchableOpacity>
+      </Animated.View>
+      <Animated.View style={[styles.circle, { bottom: icon_2, right: icon_2}]}>
+        <TouchableOpacity>
+          <MaterialCommunityIcons name="google-maps" color='white' size={25} />
+        </TouchableOpacity>
+      </Animated.View>
+      <Animated.View style={[styles.circle, { right: icon_3}]}>
+        <TouchableOpacity>
+          <MaterialCommunityIcons name="face-man-profile" color='white' size={25} />
+        </TouchableOpacity>
+      </Animated.View>
+      <TouchableOpacity
+        style={styles.circle}
+        onPress={() => {
+          pop === false ? popIn() : popOut();
+        }}
+      >
+        <MaterialCommunityIcons name="plus" color='white' size={25} />
+      </TouchableOpacity>
+    </View>
   )
+
 }
 
-const styles = StyleSheet.create({
-  View: {
-    flex: 1,
-  },
-});
-
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  circle: {
+     backgroundColor: 'black',
+     width: 60,
+     height: 60,
+     position: 'absolute',
+     bottom: 40,
+     right: 40,
+     borderRadius: 50,
+     justifyContent: 'center',
+     alignItems: 'center',
+  },
+  image: {
+    width: 400,
+    height: 700,
+    opacity: 0.5,
+  }
+})
--- src/screens/Patinetes.js
+++ src/screens/Patinetes.js
@@ -1,14 +1,96 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 
 const Patinetes = () => {
-  return (
-    <View>
-      <Text>
-        Patinetes
-      </Text>
+  const Patinetes = [
+    {
+      id: 1,
+      name: 'Patinete 1',
+      image: require('../media/patinete1.png'),
+    },
+    {
+      id: 2,
+      name: 'Patinete 2',
+      image: require('../media/patinete2.png'),
+    },
+    {
+      id: 3,
+      name: 'Patinete 3',
+      image: require('../media/patinete3.png'),
+    },
+  ];
+
+  const patinete = ( {item} ) => (
+    <View style={styles.item}>
+      <View style={styles.avatarContent}>
+        <Image source={item.image} style={styles.avatar} />
+    </View>
+    <Text style={styles.name}>{item.name}</Text>
     </View>
   )
+
+  const headerComponent = () => {
+    return <Text style={styles.listHeadline}>Patinetes</Text>
+  }
+
+  const itemSeparator = () => {
+    return <View style={styles.separator} />
+  }
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        ListHeaderComponentStyle = {styles.listHeader}
+        ListHeaderComponent = {headerComponent}
+        data = { Patinetes }
+        renderItem = { patinete}
+        ListEmptyComponent = {<Text>Prueba</Text>}
+        ItemSeparatorComponent = {itemSeparator}
+      />
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  listHeadline: {
+    color: '#333',
+    fontSize: 31,
+    fontWeight: 'bold',
+    paddingTop: 10,
+  },
+  listHeader: {
+    height: 55,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 13,
+  },
+  avatarContent: {
+    backgroundColor: '#D9D9D9',
+    borderRadius: 100,
+    height: 89,
+    width: 89,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    height: 55,
+    width: 55,
+  }, 
+  name: {
+    fontWeight: '600',
+    fontSize: 16,
+    marginLeft: 13,
+  },
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#CCC',
+  }
+});
 
 export default Patinetes;
